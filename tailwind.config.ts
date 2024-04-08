a546185488107208013a8e8e7826c6b2085e359d
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-04-08 23:57:09 +0530

    Add Hero Section.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,19 @@
 import type { Config } from 'tailwindcss'
+const {
+  default: flattenColorPalette
+} = require('tailwindcss/lib/util/flattenColorPalette')
+
+// Plugin to add each Tailwind color as a global CSS variable
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme('colors'))
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+  )
+
+  addBase({
+    ':root': newVars
+  })
+}
 
 const config: Config = {
   content: [
@@ -9,6 +24,21 @@ const config: Config = {
   darkMode: 'class',
   theme: {
     extend: {
+      animation: {
+        spotlight: 'spotlight 2s ease .75s 1 forwards'
+      },
+      keyframes: {
+        spotlight: {
+          '0%': {
+            opacity: '0',
+            transform: 'translate(-72%, -62%) scale(0.5)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translate(-50%,-40%) scale(1)'
+          }
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -16,6 +46,6 @@ const config: Config = {
       }
     }
   },
-  plugins: []
+  plugins: [addVariablesForColors]
 }
 export default config
