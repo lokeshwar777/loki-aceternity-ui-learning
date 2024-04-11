f3d1c6d2b75efcead73d01fb59e14c44b5243f97
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-04-11 21:39:35 +0530

    Add moving cards and background grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from '@/components/FeaturedCourses'
 import HeroSection from '@/components/HeroSection'
+import TestimonialCards from '@/components/TestimonialCards'
 import WhyChooseUs from '@/components/ui/WhyChooseUs'
 import Image from 'next/image'
 
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
+      <TestimonialCards />
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from 'tailwindcss'
 import { default as flattenColorPalette } from 'tailwindcss/lib/util/flattenColorPalette'
 
+import svgToDataUri from 'mini-svg-data-uri'
+
+import colors from 'tailwindcss/colors'
+
 // Plugin to add each Tailwind color as a global CSS variable
 function addVariablesForColors({ addBase, theme }: any) {
   const allColors = flattenColorPalette(theme('colors'))
@@ -13,6 +17,29 @@ function addVariablesForColors({ addBase, theme }: any) {
   })
 }
 
+function addSvgPatterns({ matchUtilities, theme }: any) {
+  matchUtilities(
+    {
+      'bg-grid': (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`
+      }),
+      'bg-grid-small': (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`
+      }),
+      'bg-dot': (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+        )}")`
+      })
+    },
+    { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+  )
+}
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -23,7 +50,9 @@ const config: Config = {
   theme: {
     extend: {
       animation: {
-        spotlight: 'spotlight 2s ease .75s 1 forwards'
+        spotlight: 'spotlight 2s ease .75s 1 forwards',
+        scroll:
+          'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite'
       },
       keyframes: {
         spotlight: {
@@ -35,6 +64,11 @@ const config: Config = {
             opacity: '1',
             transform: 'translate(-50%,-40%) scale(1)'
           }
+        },
+        scroll: {
+          to: {
+            transform: 'translate(calc(-50% - 0.5rem))'
+          }
         }
       },
       backgroundImage: {
@@ -44,6 +78,6 @@ const config: Config = {
       }
     }
   },
-  plugins: [addVariablesForColors]
+  plugins: [addVariablesForColors, addSvgPatterns]
 }
 export default config
